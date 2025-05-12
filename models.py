26e0ded19eba9c5ccd35e11d89d990ddb24aec76
dansana <dsanabrias2003@gmail.com>
2025-05-12 16:31:42 +0200

    modelos y rutas

---
+++ models.py
@@ -0,0 +1,6 @@
+from .app import db
+from datetime import datetime
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
